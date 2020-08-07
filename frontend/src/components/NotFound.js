f0d72c2547093661bd67cc552ac580b7097415c1
pornmanut <pornmanuty@gmail.com>
2020-08-07 22:10:56 +0700

    add 404 from somewhere website

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-        <div>
-            Nothing to see here
+        <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="error-template">
+                    <h1>
+                        Oops!</h1>
+                    <h2>
+                        404 Not Found</h2>
+                    <div class="error-details">
+                        Sorry, an error has occured, Requested page not found!
+                    </div>
+                    <div class="error-actions">
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
     )
 }
 
